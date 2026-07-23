df544851bcd7008a3f6c9a044d89a5193a96670b
martamartinic <marta.martinic29@gmail.com>
2026-07-23 23:37:07 +0200

    dodavanje Capacitor mapa bez android platoforme

---
+++ src-capacitor/capacitor.config.js
@@ -0,0 +1,6 @@
+const { defineCapacitorConfig } = require('@quasar/app-vite/capacitor');
+
+module.exports = defineCapacitorConfig({
+  appId: 'hr.martamartinic.gymmetrics',
+  appName: 'GymMetrics'
+});
