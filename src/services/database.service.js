adde6150dd684fd657a85914a1ec175e1f14e503
mmartamartinic <marta.martinic29@gmail.com>
2026-07-31 23:56:34 +0200

    dodane create table naredbe u database.service.js u mapi src/services

--- src/services/database.service.js
+++ src/services/database.service.js
@@ -445,21 +445,12 @@ class DatabaseService {
       'F. Baza gymMetrics otvorena.'
     );
 
-    //izvršavaju se naredbe za kreiranje tablica
+    await this.db.execute('PRAGMA foreign_keys = ON'); //uključuje provjeru FK ograničenja
+    console.log('F.1 FK provjera uključena');
 
-    await this.db.execute(`
-      CREATE TABLE IF NOT EXISTS Testna_tablica (
-        ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        Poruka TEXT NOT NULL
-      );
-    `); 
+    await this.createTables();
 
-    //await this.db.execute('PRAGMA foreign_keys = ON'); //uključuje provjeru FK ograničenja
-    //console.log('F.1 FK provjera uključena');
-
-    //await this.createTables();
-
-    console.log('G. CREATE TABLE naredbe završene.');
+    console.log('G. Sve SQLite tablice uspješno kreirane/provjerene.');
 
     // Dohvaćamo popis tablica koje postoje u bazi iz SQLite plugin-a
     const tables = await this.db.getTableList();
@@ -472,13 +463,320 @@ class DatabaseService {
   
 
    /*         KREIRANJE TABLICA BAZE       */
-  /*
+  
   async createTables() {
 
-    //CREATE TABLE NAREDBE.
+    //TRENERI
+    await this.db.execute(` 
+
+        CREATE TABLE IF NOT EXISTS Treneri (
+            ID_trenera INTEGER PRIMARY KEY AUTOINCREMENT,
+            Ime_trenera TEXT NOT NULL,
+            Prezime trenera TEXT NOT NULL,
+            Email TEXT NOT NULL UNIQUE,
+            Lozinka TEXT NOT NULL
+        
+        ); 
+    `);  //trener tablica zagrada
+
+
+    await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Klijenti (
+                ID_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_trenera INTEGER NOT NULL,
+                Ime_klijenta TEXT NOT NULL,
+                Prezime_klijenta TEXT NOT NULL,
+                Spol TEXT NOT NULL,
+                Kontakt TEXT NOT NULL,
+                Visina_cm REAL NOT NULL,
+                Pocetna_tezina_kg REAL NOT NULL,
+                Iskustvo TEXT NOT NULL,
+                Napomena TEXT,
+                Aktivan_flag INTEGER NOT NULL DEFAULT 1,
+
+                FOREIGN KEY (ID_trenera)
+                    REFERENCES Treneri(ID_trenera)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            );
+
+       ` ); //zagrada await this.db.execute tablica klijenti
+
+        await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Grupa_ciljeva (
+                ID_ciljeva INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Naziv_cilja TEXT NOT NULL,
+                Opis_cilja TEXT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica GRUPA CILJEVA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vrsta_mjerenja (
+                ID_vrsta_mjerenja INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Naziv_mjerenja TEXT NOT NULL,
+                Mjerna_jedinica TEXT NOT NULL
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VRSTA MJERENJA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vrsta_testova (
+                ID_vrsta_testa INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Naziv_testa TEXT NOT NULL,
+                Mjerna_jedinica_testa TEXT NOT NULL
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VRSTA TESTOVA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vrsta_treninga (
+                ID_vrsta_treninga INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Vrsta_treninga TEXT NOT NULL
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VRSTA TRENINGA
+
+        await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vjezbe (
+                ID_vjezbe INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Naziv_vjezbe TEXT NOT NULL,
+                Misicna_skupina TEXT NOT NULL,
+                Default_vjezba INTEGER NOT NULL DEFAULT 0
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VJEŽBE
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Parametar_krvne_slike (
+                ID_parametra_krvne_slike INTEGER PRIMARY KEY AUTOINCREMENT, 
+                Naziv_parametra_ks TEXT NOT NULL,
+                Jedinica_parametra_ks TEXT NOT NULL,
+                Ref_min REAL NOT NULL,
+                Ref_max REAL NOT NULL
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica PARAMETAR KS
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Prehrana (
+                ID_prehrane INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                Dnevne_kalorije_kcal INTEGER NOT NULL,
+                Proteini_g INTEGER,
+                Ugljikohidrati_g INTEGER,
+                Secer_g INTEGER,
+                Masti_g INTEGER,
+                Vlakna_g INTEGER,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica PREHRANA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Zdravstveno_stanje (
+                ID_zd_stanja INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                Dijagnoza TEXT,
+                Lijek TEXT,
+                Ozljeda TEXT,
+                Bolovi TEXT,
+                Sistolicki_tlak INTEGER,
+                Dijastolicki_tlak INTEGER,
+                Datum_zd_stanja TEXT NOT NULL,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica ZD STANJE
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Ciljevi_klijenta (
+                ID_ciljeva_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                ID_ciljeva INTEGER NOT NULL,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+                
+                FOREIGN KEY (ID_ciljeva)
+                    REFERENCES Grupa_ciljeva(ID_ciljeva)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica CILJEVI KLIJENTA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Mjerenja_klijenta (
+                ID_mjere_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                ID_vrsta_mjerenja INTEGER NOT NULL,
+                Vrijednost_mjerenja REAL NOT NULL,
+                Datum_mjerenja TEXT NOT NULL,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+                
+                FOREIGN KEY (ID_vrsta_mjerenja)
+                    REFERENCES Vrsta_mjerenja(ID_vrsta_mjerenja)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica MJERENJA KLIJENTA
+
+         await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Testovi_klijenta (
+                ID_testa_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                ID_vrsta_testa INTEGER NOT NULL,
+                Rezultat_testa REAL NOT NULL,
+                Datum_testa TEXT NOT NULL,
+                Biljeska_testa TEXT,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+                
+                FOREIGN KEY (ID_vrsta_testa)
+                    REFERENCES Vrsta_testova(ID_vrsta_testa)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica TESTOVI KLIJENTA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Trening_klijenta (
+                ID_treninga_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                ID_vrsta_treninga INTEGER NOT NULL,
+                Datum_treninga TEXT NOT NULL,
+                Ocjena_treninga REAL NOT NULL,
+                Biljeska_treninga TEXT,
+
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+                
+                FOREIGN KEY (ID_vrsta_treninga)
+                    REFERENCES Vrsta_treninga(ID_vrsta_treninga)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica TRENING KLIJENTA
+
+          await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vjezba_vrste_treninga (
+                ID_vjezba_vrste_treninga INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_vrsta_treninga INTEGER NOT NULL,
+                ID_vjezbe INTEGER NOT NULL,
+                
+                FOREIGN KEY (ID_vrsta_treninga)
+                    REFERENCES Vrsta_treninga(ID_vrsta_treninga)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT,
+
+                FOREIGN KEY (ID_vjezbe)
+                    REFERENCES Vjezbe(ID_vjezbe)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VJEŽBA VRSTE TRENINGA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vjezbe_treninga_klijenta (
+                ID_vjezbe_treninga_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_treninga_klijenta INTEGER NOT NULL,
+                ID_vjezbe INTEGER NOT NULL,
+                
+                FOREIGN KEY (ID_treninga_klijenta)
+                    REFERENCES Trening_klijenta(ID_treninga_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+
+                FOREIGN KEY (ID_vjezbe)
+                    REFERENCES Vjezbe(ID_vjezbe)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VJEŽBE TRENINGA KLIJENTA
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Vrijednost_krvne_slike (
+                ID_vrijednosti_krvne_slike INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_klijenta INTEGER NOT NULL,
+                ID_parametra_krvne_slike INTEGER NOT NULL,
+                Datum_nalaza_ks TEXT NOT NULL,
+                Vrijednost_krvne_slike REAL,
+                
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+
+                FOREIGN KEY (ID_parametra_krvne_slike)
+                    REFERENCES Parametar_krvne_slike(ID_parametra_krvne_slike)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica VRIJEDNOST KS
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Termini (
+                ID_termina INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_trenera INTEGER NOT NULL,
+                Datum_termina TEXT NOT NULL,
+                Vrijeme_termina TEXT NOT NULL,
+                
+                FOREIGN KEY (ID_trenera)
+                    REFERENCES Treneri(ID_trenera)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica TERMINI
+
+       await this.db.execute( `
+            CREATE TABLE IF NOT EXISTS Termini_klijenta (
+                ID_termina_klijenta INTEGER PRIMARY KEY AUTOINCREMENT, 
+                ID_termina INTEGER NOT NULL,
+                ID_klijenta INTEGER NOT NULL,
+                
+                FOREIGN KEY (ID_termina)
+                    REFERENCES Termini(ID_termina)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+                
+                FOREIGN KEY (ID_klijenta)
+                    REFERENCES Klijenti(ID_klijenta)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+            ); 
+
+       ` ); //zagrada await this.db.execute tablica TERMINI KLIJENTA
+
 
   } //async createTables zagrada
-   */
+   
 
 } //Class DatabaseService zagrada
 
